b6790c907ead377fc29ca3509b0b03b6d7da9a06
bachatabarcelona <bachatabarcelonaoficial@gmail.com>
2026-01-24 05:30:28 +0100

    Delete media-2026-01-11-domingo.js

--- media-2026-01-11-domingo.js
+++
@@ -1,8 +0,0 @@
-const media = [
-  { type: "img", src: "fotos/ENERO-2026/DOMINGOS/2026-01-11-domingo/2.692_10.jpg" },
-  { type: "img", src: "fotos/ENERO-2026/DOMINGOS/2026-01-11-domingo/2.706_8.jpg" },
-  { type: "img", src: "fotos/ENERO-2026/DOMINGOS/2026-01-11-domingo/2.769_8.jpg" }
-
-  // aquí luego irán también vídeos:
-  // { type: "video", src: "videos/ENERO-2026/DOMINGOS/2026-01-11-domingo/1.mp4" }
-];
